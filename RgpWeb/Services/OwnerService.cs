d9d7212191bffea11207013e1f2f0b18b6c0be7a
Liga <ligacepurniece@gmail.com>
2022-11-25 20:19:31 +0200

    Update OwnerService

--- RgpWeb/Services/OwnerService.cs
+++ RgpWeb/Services/OwnerService.cs
@@ -19,8 +19,9 @@ namespace RgpWeb.Services
             foreach (var owner in userList)
             {
                 var area = _context.Units.Where(unit => unit.Owner.Id == owner.Id).Sum(unit => unit.Area);
+                var propertyCount = _context.Properties.Count(p => p.Owner.Id == owner.Id);
 
-                userListWithArea.Add(new OwnerRequestModel(owner.Id, owner.FullName, owner.RegNumber, owner.OwnerType, area));
+                userListWithArea.Add(new OwnerRequestModel(owner.Id, owner.FullName, owner.RegNumber, owner.OwnerType, area, propertyCount));
             }
 
             return userListWithArea.OrderBy(user => user.OwnerName);
